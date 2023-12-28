9fba738178f936a8cd74def4fc854dace633aa1a
PolaccoPablo <136521878+PolaccoPablo@users.noreply.github.com>
2023-12-28 14:05:45 -0300

    Update CartView.jsx con la tabla que arme para visualizar los detalles

--- ecomerce-pdl/src/components/CartView/CartView.jsx
+++ ecomerce-pdl/src/components/CartView/CartView.jsx
@@ -1,10 +1,11 @@
+import './CartView.css'
 import { UsecartContext } from '../Contex/CartContex'
 import { Link } from 'react-router-dom'
 import CartItem from '../CartItem/CartItem'
 
 export default function CartView() {
     const { cart, clearCart, cantTotalCart, precioTotalCart } = UsecartContext()
-    console.log(cantTotalCart())
+    
     if (cantTotalCart() === 0) {
         return (
         <div className='card'>
@@ -12,22 +13,27 @@ export default function CartView() {
             <Link to='/' className='Option'>productos</Link>
         </div>
     )}
-
-
-
-    return (
+return (
 <div className='card'>
-    {cart.map(p => <CartItem key={p.id} {...p}/>)}
+<div className='ItemTable'>
+      <table >
+      <thead>
+          <tr>
+          <th>Nombre</th>
+          <th>Precio unit.</th>
+          <th>Cantidad</th>
+          <th>SubTotal</th>
+          </tr>
+        </thead>
+        {cart.map(p => <CartItem key={p.id} {...p}/>)}
+      </table>
+    </div>           
         <h3>Total: ${precioTotalCart()}</h3>
         <footer>
         <button className="btn btn-danger" onClick={() => clearCart()} > Limpiar carrito</button>
         <Link to='/'><button className='btn btn-secondary'>Seguir Comprando</button></Link>
         <Link to='/checkout'><button className='btn btn-success'>Checkout</button></Link>
         </footer>
-
 </div>
-
-
-
   )
 }
