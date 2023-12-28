a2262bf973401787f9e0768989b7b9475d0f7973
PolaccoPablo <136521878+PolaccoPablo@users.noreply.github.com>
2023-12-28 14:07:53 -0300

    Complete la tabla de los detalles en  CartItem.jsx

--- ecomerce-pdl/src/components/CartItem/CartItem.jsx
+++ ecomerce-pdl/src/components/CartItem/CartItem.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
-
-
-export default function CartItem() {
+export default function CartItem({id, nombre, precio, quantity}) {
   return (
-    <div>aca falta completar con los datos reales estamos en CartItem</div>
+
+      <tbody>
+        <tr>
+          <td>{nombre}</td>
+          <td>{precio}</td>
+          <td>{quantity}</td>
+          <td>{precio*quantity}</td>
+        </tr>
+      </tbody>
+
   )
 }
